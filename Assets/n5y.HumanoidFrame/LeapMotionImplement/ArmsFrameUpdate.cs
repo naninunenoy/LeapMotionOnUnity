22673c335db6b138a880e905e6990ed78572c89d
naninunenoy <naninuneno0411@gmail.com>
2020-05-20 21:56:12 +0900

    UniTask2

---
+++ Assets/n5y.HumanoidFrame/LeapMotionImplement/ArmsFrameUpdate.cs
@@ -0,0 +1,9 @@
+﻿using n5y.HumanoidFrame.Abstruct;
+using Cysharp.Threading.Tasks;
+
+namespace n5y.HumanoidFrame.LeapMotionImplement
+{
+    public class ArmsFrameUpdate : IArmsFrameUpdate
+    {
+    }
+}
