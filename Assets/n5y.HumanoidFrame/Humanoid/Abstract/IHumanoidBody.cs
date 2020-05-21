0195ce7bec22347d3f74ceede6d6070d9be707e9
naninunenoy <naninuneno0411@gmail.com>
2020-05-22 00:11:21 +0900

    Animation Rigging

---
+++ Assets/n5y.HumanoidFrame/Humanoid/Abstract/IHumanoidBody.cs
@@ -0,0 +1,9 @@
+﻿using n5y.HumanoidFrame.Dto;
+
+namespace n5y.HumanoidFrame.Humanoid.Abstract
+{
+    public interface IHumanoidBody
+    {
+        void ApplyArms(Arms arms);
+    }
+}
---
+++ Assets/n5y.HumanoidFrame/Humanoid/RiggingImplement/RiggingHumanoidBody.cs
@@ -0,0 +1,25 @@
+﻿using n5y.HumanoidFrame.Dto;
+using n5y.HumanoidFrame.Humanoid.Abstract;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+namespace n5y.HumanoidFrame.Humanoid.RiggingImplement
+{
+    public class RiggingHumanoidBody : IHumanoidBody
+    {
+        readonly Animator animator;
+        readonly RigBuilder rigBuilder;
+        
+        public RiggingHumanoidBody(Animator animator, RigBuilder rigBuilder)
+        {
+            this.animator = animator;
+            this.rigBuilder = rigBuilder;
+        }
+        
+        
+        public void ApplyArms(Arms arms)
+        {
+            
+        }
+    }
+}
